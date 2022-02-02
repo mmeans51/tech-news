396ea3084ad21522d616e8498169c60df8736d5a
michael Means <colemeans51@gmail.com>
2022-02-02 14:57:52 -0600

    save

--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -49,12 +49,17 @@ router.post('/', (req, res) => {
     });
 });
 
+router.post('/login', (req, res) => {
+  
+})
+
 //put /api/users/1
 router.put('/:id', (req, res) => {
     // expects {username: 'Lernantino', email: 'lernantino@gmail.com', password: 'password1234'}
 
   // if req.body has exact key/value pairs to match the model, you can just use `req.body` instead
   User.update(req.body, {
+    individualHooks: true,
     where: {
       id: req.params.id
     }
